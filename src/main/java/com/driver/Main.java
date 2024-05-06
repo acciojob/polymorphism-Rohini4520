504b7d77ed1bfc1c2874fa8157e6e92fa3161c23
Your Name <you@example.com>
2024-05-06 22:33:48 +0530

    Assignment submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
+public class Main{
+public static void main(String[],args) {
+    Product p = new Product();
 
-public class Main {
+    int ans = p.product(x: 10, y: 20);
+    int ans1 = p.product(x: 10, y:2 0, z: 30);
+
+    int ans3 = p.product(x: 1.0, y: 2.0);
+
+    System.out.println(ans+" "+ans1+" "+ans3);
 
 }
+
+
+   static class Product {
+       public int product(int x, int y) {
+       return x*y;
+
+       }
+       public int product(int x, int y, int z) {
+       return x*y*z;
+       }
+
+       public double product(double x, double y) {
+           return  x*y;
+       }
+   }
+   }
+
+
